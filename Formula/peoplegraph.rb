f1877c839d87883e479db7255a3da955b089a3ee
kayacancode <62274079+kayacancode@users.noreply.github.com>
2026-05-22 07:15:32 -0400

    Update peoplegraph to 0.3.0

--- Formula/peoplegraph.rb
+++ Formula/peoplegraph.rb
@@ -1,8 +1,8 @@
 class Peoplegraph < Formula
   desc "Query Obsidian Gmail CRM contact graph from a local or remote cache"
   homepage "https://github.com/kayacancode/obsidian-gmail-crm"
-  url "https://github.com/kayacancode/obsidian-gmail-crm/releases/download/peoplegraph-v0.2.1/peoplegraph-0.2.1.tar.gz"
-  sha256 "7cc2fa11c92374e803a04605fcf7fb761814cce6d59789587f6246e252a609ad"
+  url "https://github.com/kayacancode/obsidian-gmail-crm/releases/download/peoplegraph-v0.3.0/peoplegraph-0.3.0.tar.gz"
+  sha256 "389efb28e8815b3099fca9e089c29807afc25e0c5281d75468e624d0eb709a86"
   license "MIT"
 
   depends_on "rust" => :build
